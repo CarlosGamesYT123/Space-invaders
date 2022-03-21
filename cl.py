fcb1f1381dfeb8395b40e47cc1678b5c4664bdfa
Hedgehawk11 <84346970+dbates314@users.noreply.github.com>
2022-03-21 08:38:45 -0700

    Update cl.py

--- cl.py
+++ cl.py
@@ -1,2 +1,21 @@
-import pygame
-pygame.init
+
+
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode([800, 800])
+pygame.display.set_caption("Space Invaders")
+background = pygame.image.load("background.png")
+background = pygame.transform.scale(background, (800, 600))
+playerImg = pygame.image.load("player.png")
+playerImg = pygame.transform.scale(playerImg, (64, 64))
+playerImg2 = pygame.image.load("player2.png")
+playerImg2 = pygame.transform.scale(playerImg2, (64, 64))
+playerImg3 = pygame.image.load("player3.png")
+playerImg3 = pygame.transform.scale(playerImg3, (64, 64))
+playerImg4 = pygame.image.load("player4.png")
+playerImg4 = pygame.transform.scale(playerImg4, (64, 64))
+playerImg5 = pygame.image.load("player5.png")
+playerImg5 = pygame.transform.scale(playerImg5, (64, 64))
+playerwinner = pygame.image.load("winner.png")
+playerwinner = pygame.transform.scale(playerwinner, (64, 64))
+
